d61c58a7877708425c20d2d31d0612fa55fd6500
覃晓旭 <qxx@e.hunantv.com>
2016-05-04 10:03:55 +0800

    初始文档

---
+++ app/src/main/java/com/mgtv/qxx/ttsdemo/TtsDemoActivity.java
@@ -0,0 +1,13 @@
+package com.mgtv.qxx.ttsdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TtsDemoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tts_demo);
+    }
+}
