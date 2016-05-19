3e04d9745c9e30f3b37b81cbf3d5d6fcb5a5174e
qxs820624 <qxs820624@sina.com>
2016-05-19 23:51:20 +0800

    添加tessert的库

--- app/src/main/java/com/mgtv/qxx/ttsdemo/OcrActivity.java
+++ app/src/main/java/com/mgtv/qxx/ttsdemo/OcrActivity.java
@@ -1,14 +1,23 @@
 package com.mgtv.qxx.ttsdemo;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import com.googlecode.tesseract.android.TessBaseAPI;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.File;
 
 public class OcrActivity extends AppCompatActivity {
 
+    private TextView text;
+    TessBaseAPI baseApi;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +33,47 @@ public class OcrActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-    }
+        Button bt=new Button(getBaseContext());
+        bt=(Button)findViewById(R.id.button1);
 
+        text=new TextView(getBaseContext());
+        text=(TextView)findViewById(R.id.textView1);
+
+        baseApi=new TessBaseAPI();
+        baseApi.init("/mnt/sdcard/tesseract/", "eng");
+
+        bt.setOnClickListener(new OnClickListener() {
+                                  @Override
+                                  public void onClick(View sourse) {
+                                      // text.setText("sb");
+                                      //设置要ocr的图片bitmap
+                                      baseApi.setImage(getDiskBitmap("/mnt/sdcard/mypic.bmp"));
+                                      //根据Init的语言，获得ocr后的字符串
+                                      String text1= baseApi.getUTF8Text();
+                                      text.setText(text1);
+                                      //释放bitmap
+                                      baseApi.clear();
+                                  }
+                              }
+        );
+    }
+    private Bitmap getDiskBitmap(String pathString)
+    {
+        Bitmap bitmap = null;
+        try
+        {
+            File file = new File(pathString);
+            if(file.exists())
+            {
+                bitmap = BitmapFactory.decodeFile(pathString);
+
+            }
+        } catch (Exception e)
+        {
+            // TODO: handle exception
+        }
+
+
+        return bitmap;
+    }
 }
