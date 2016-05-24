b82c9febce59098798b989b60ed6ee2ba8ef6a2f
qxx <qxs820624@sina.com>
2016-05-24 23:39:39 +0800

    修改图像识别布局

--- app/src/main/java/com/mgtv/qxx/ttsdemo/OcrActivity.java
+++ app/src/main/java/com/mgtv/qxx/ttsdemo/OcrActivity.java
@@ -1,8 +1,13 @@
 package com.mgtv.qxx.ttsdemo;
 
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -10,17 +15,25 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.googlecode.leptonica.android.JpegIO;
 import com.googlecode.tesseract.android.TessBaseAPI;
 
 import java.io.File;
+import java.io.StreamCorruptedException;
 
 public class OcrActivity extends AppCompatActivity {
 
-    private TextView text;
+    private TextView textRecognitionResult;
     TessBaseAPI baseApi;
     private String picFile;
+    private EditText etSelectPicture;
+    private Button btnSelectPic;
+
+    private static final int ACTIVITY_GET_CONTENT = 10;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,10 +44,14 @@ public class OcrActivity extends AppCompatActivity {
 
         //接收name值
         picFile  = extras.getString("picture_path");
-        Log.e("OcrActivity",picFile);
+        // Log.e("OcrActivity",picFile);
+        etSelectPicture = (EditText)findViewById(R.id.et_select_picture);
+        etSelectPicture.setText(picFile);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        /*
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,29 +60,50 @@ public class OcrActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-        Button bt=new Button(getBaseContext());
-        bt=(Button)findViewById(R.id.button1);
+        */
 
-        text=new TextView(getBaseContext());
-        text=(TextView)findViewById(R.id.textView1);
+        btnSelectPic = (Button) findViewById(R.id.btn_select_picture);
+
+        Button btRecognition=new Button(getBaseContext());
+        btRecognition=(Button)findViewById(R.id.btn_recognition);
+
+        textRecognitionResult=new TextView(getBaseContext());
+        textRecognitionResult=(TextView)findViewById(R.id.tv_recognition_result);
 
         baseApi=new TessBaseAPI();
-        baseApi.init("/mnt/sdcard2/tesseract/", "eng");
+        boolean bInitBaseApi = baseApi.init("/sdcard2/tesseract/", "eng");
+        if (!bInitBaseApi)  {
+            Log.e("OCRActivity", "baseApi init Failed!");
+            Toast.makeText(this, this.getResources().getString(R.string.ocr_init_faile_prompt), Toast.LENGTH_LONG).show();
+            finish();
+        }
 
-        bt.setOnClickListener(new View.OnClickListener() {
-                                  @Override
-                                  public void onClick(View sourse) {
-                // text.setText("sb");
-                //设置要ocr的图片bitmap
-                baseApi.setImage(getDiskBitmap(picFile));
-                //根据Init的语言，获得ocr后的字符串
-                String text1= baseApi.getUTF8Text();
-                text.setText(text1);
-                //释放bitmap
-                baseApi.clear();
-            }
+        btRecognition.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View sourse) {
+                    //设置要ocr的图片bitmap
+                    Bitmap bm = getDiskBitmap(picFile);
+                    if (bm != null){
+                        baseApi.setImage(bm);
+                        //根据Init的语言，获得ocr后的字符串
+                        String text1= baseApi.getUTF8Text();
+                        textRecognitionResult.setText(text1);
+                    }else {
+                        textRecognitionResult.setText("SBBBBBBBBBBBBBBBBBBBB");
+                    }
+                    //释放bitmap
+                    baseApi.clear();
+                }
             }
         );
+        btnSelectPic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new  Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/bmp");
+                startActivityForResult(intent,ACTIVITY_GET_CONTENT);
+            }
+        });
     }
 
     private Bitmap getDiskBitmap(String pathString)
@@ -77,7 +115,8 @@ public class OcrActivity extends AppCompatActivity {
             if(file.exists())
             {
                 bitmap = BitmapFactory.decodeFile(pathString);
-
+            }else{
+                Toast.makeText(this,"file not exists",Toast.LENGTH_LONG).show();
             }
         } catch (Exception e)
         {
@@ -86,5 +125,34 @@ public class OcrActivity extends AppCompatActivity {
 
 
         return bitmap;
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if (ACTIVITY_GET_CONTENT == requestCode){
+            // Log.e("onActivityResult Code",String.valueOf(resultCode));
+            // Log.e("onActivityResult data",data.getData().toString());
+            if(resultCode  == RESULT_OK){
+                //得到文件的Uri
+                Uri uri = data.getData();
+                ContentResolver resolver = getContentResolver();
+                //ContentResolver对象的getType方法可返回形如content://的Uri的类型
+                //如果是一张图片，返回结果为image/jpeg或image/png等
+                String fileType = resolver.getType(uri);
+                if (fileType == null || fileType.isEmpty()){
+                    String path = uri.getPath();
+                    fileType = path.substring(path.lastIndexOf(".")+1,path.length());
+                }
+                if(fileType.startsWith("image"))//判断用户选择的是否为图片
+                {
+                    //根据返回的uri获取图片路径
+                    Cursor cursor = resolver.query(uri,  new String[]{MediaStore.Images.Media.DATA}, null, null, null);
+                    cursor.moveToFirst();
+                    String  path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+
+                    //do  anything you want
+                    etSelectPicture.setText(path);
+                }
+            }
+        }
     }
 }
